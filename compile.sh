3ea68584c6af8037e9e68435a9686839de5f36a1
Julian <cahill.julian@gmail.com>
2014-12-13 06:53:32 +1300

    files files files

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ vector3.cpp camera.cpp framework3D.cpp basescene.cpp resourcemanager.cpp main.cpp -lglfw3 -lGL -lGLU -lGLEW -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lpng
+g++ src/vector3.cpp src/camera.cpp src/framework3D.cpp src/basescene.cpp src/resourcemanager.cpp src/main.cpp -lglfw3 -lGL -lGLU -lGLEW -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lpng
